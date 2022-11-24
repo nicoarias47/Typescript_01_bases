5199d02e0bedd0af5ed9cbf98adf276665dc2b9d
nicoarias47 <ariasn1995@gmail.com>
2022-11-24 13:18:35 -0300

    build

---
+++ dist/d58a8ddf9d2c9c2f3c62.ts
@@ -0,0 +1,5 @@
+define(["require", "exports", "./components/NavBar", "./components/pokemon/PokemonContainer", "./style.css"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    const app = document.getElementById("app");
+});
